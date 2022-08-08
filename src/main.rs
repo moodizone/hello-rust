3261a821c94b910bdb9316edb4cf4bc0ace2ca9b
moodi <moodizone@gmail.com>
2022-08-08 21:51:23 +0430

    🚧 rs15

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod rs15;
+
+fn main() {
+    rs15::rs15()
+}
---
+++ src/rs15.rs
@@ -0,0 +1,20 @@
+enum Ticket {
+    BackStage(f64, String),
+    VIP(f64, String),
+    Standard(f64),
+}
+pub fn rs15() {
+    let tickets: Vec<Ticket> = vec![
+        Ticket::BackStage(1.1, "babak".to_owned()),
+        Ticket::VIP(1.2, "vahid".to_owned()),
+        Ticket::Standard(1.3),
+    ];
+
+    for ticket in tickets {
+        match ticket {
+            Ticket::Standard(amount) => println!("vector price is {:?}", amount),
+            Ticket::BackStage(amount, holder) => println!("{:?} price is {:?}", holder, amount),
+            Ticket::VIP(amount, holder) => println!("{:?} price is {:?}", holder, amount),
+        }
+    }
+}
